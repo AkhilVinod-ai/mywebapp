ed723eda60c34fa23e131b6e2b54ea70e23e91f8
akhilvinod.ai <akhilvinodk@gmail.com>
2025-11-05 14:13:45 -0800

    Update in the UI

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./App.css"; // reuse the cyber background
 import { Authenticator } from "@aws-amplify/ui-react";
-ReactDOM.createRoot(document.getElementById("root")).render(
- <React.StrictMode>
- <Authenticator>
- <App />
- </Authenticator>
- </React.StrictMode>
-);
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <div className="cyber-bg">
+      <div className="cyber-overlay"></div>
+      <div className="auth-wrapper">
+        <Authenticator>
+          <App />
+        </Authenticator>
+      </div>
+    </div>
+  </React.StrictMode>
+);
